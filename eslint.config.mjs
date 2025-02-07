bb4c671e710e024df30ef87f89d0a0fa9b9d075e
Nguyen Phuc <nguyenhuuphuc22052004@gmail.com>
2025-02-07 16:58:58 +0700

    chore(config): add ESLint, Prettier, Git and other settings - Add .eslintrc, .prettierrc, and .gitignore - Include nodemon.json and tsconfig.json - Configure Yarn and package dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,62 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintPluginJsdoc from 'eslint-plugin-jsdoc'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+      jsdoc: eslintPluginJsdoc
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'prettier/prettier': [
+        'warn',
+        {
+          arrowParens: 'always',
+          semi: false,
+          trailingComma: 'none',
+          tabWidth: 2,
+          endOfLine: 'auto',
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: true
+        }
+      ],
+
+      'jsdoc/check-alignment': 'error',
+      'jsdoc/check-param-names': 'error',
+      'jsdoc/check-tag-names': 'error',
+      'jsdoc/check-types': 'error',
+      'jsdoc/require-jsdoc': [
+        'warn',
+        {
+          require: {
+            FunctionDeclaration: true,
+            MethodDefinition: true,
+            ArrowFunctionExpression: false,
+            ClassDeclaration: true
+          }
+        }
+      ],
+      'jsdoc/require-param': 'warn',
+      'jsdoc/require-param-description': 'warn',
+      'jsdoc/require-param-type': 'warn',
+      'jsdoc/require-returns': 'warn',
+      'jsdoc/require-returns-description': 'warn',
+      'jsdoc/require-returns-type': 'warn'
+    }
+  },
+  {
+    ignores: ['**/node_modules/', '**/dist/']
+  }
+]
