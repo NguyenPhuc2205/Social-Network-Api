cb8ec8544289c562aa074cd4e8106e040c14cad9
Nguyen Phuc <nguyenhuuphuc22052004@gmail.com>
2025-02-16 23:48:10 +0700

    feat(middleware): add morgan, helmet, and compression middlewares
    - Integrated \morgan('dev')\ for logging HTTP requests.
    - Added \helmet()\ to enhance security by setting various HTTP headers.
    - Enabled \compression()\ to improve response performance by compressing HTTP responses.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import express from 'express'
+import express, { RequestHandler } from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import compression from 'compression'
 import usersRouter from './routes/users.routes'
 import databaseService from './services/database.services'
 import { defaultErrorsHandler } from './middlewares/error.middlewares'
-import { EmailRequestBody, sendingEmail } from './utils/email'
+import { ErrorRequestHandler } from 'express-serve-static-core'
 
 const app = express()
 const port = 3000
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(compression() as RequestHandler)
 
 /**
  * Middlewar parses resquest body from 'json' type to 'javascript object'
@@ -36,8 +43,3 @@ app.listen(port, () => {
   console.log('Server is running on port 3000')
 })
 
-import { Request, Response } from 'express'
-import core from 'express-serve-static-core'
-app.post('/email/send', (req: Request<core.ParamsDictionary, any, EmailRequestBody>, res) => {
-  sendingEmail(req, res)
-})
