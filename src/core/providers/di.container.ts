18901a43e0395d2f35723f6dcb560ca1023e74cb
Phuc Nguyen <nguyenhuuphuc22052004@gmail.com>
2025-03-21 23:44:10 +0700

    feat(provider): add new dependency injection types

--- src/core/providers/di.container.ts
+++ src/core/providers/di.container.ts
@@ -1,13 +1,14 @@
-import { Container } from 'inversify'
-import TYPES from './di.types'
-import UserService from '~/modules/users/services/user.service'
-import UserRepository from '~/modules/users/repositories/user.repository'
-import UserController from '~/modules/users/presentation/http/user.controller'
 
-const container = new Container()
+import { Container } from 'inversify';
+import { DI_TYPES } from './di.types';
+import { WinstonLogger } from '~/infrastructure/loggers/winston.log';
 
-container.bind<UserService>(TYPES.UserService).to(UserService)
-container.bind<UserRepository>(TYPES.UserRepository).to(UserRepository)
-container.bind<UserController>(TYPES.UserController).to(UserController)
+export const createDIContainer = (): Container => {
+    const container = new Container({
+        defaultScope: "Singleton",
+    })
 
-export { container }
+    container.bind(DI_TYPES.Logger).to(WinstonLogger).inSingletonScope()
+
+    return container
+}
