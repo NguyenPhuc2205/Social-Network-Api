6e162a447a31452ab7053160cfdecd9b613ec1cb
Phuc Nguyen <nguyenhuuphuc22052004@gmail.com>
2025-06-24 21:47:05 +0700

    feat(shared): add shared types include: zod-validator type and translation keys

--- src/shared/types/translation-keys.type.ts
+++ src/shared/types/translation-keys.type.ts
@@ -1,25 +1,34 @@
 /*
- * @Author: Phuc Nguyen nguyenhuuphuc22052004@gmail.com
- * @Date: 2025-03-17 10:20:32
- * @LastEditors: Phuc Nguyen nguyenhuuphuc22052004@gmail.com
- * @LastEditTime: 2025-05-22 14:20:28
- * @FilePath: /server/src/shared/types/translation-keys.type.ts
- * @Description: Type definitions for translation keys used in i18n
+ * @Author        : Phuc Nguyen nguyenhuuphuc22052004@gmail.com
+ * @Date          : 2025-05-06 21:43:17
+ * @LastEditors   : Phuc Nguyen nguyenhuuphuc22052004@gmail.com
+ * @LastEditTime  : 2025-05-24 08:34:20
+ * @FilePath      : /project/server/src/shared/types/translation-keys.type.ts
+ * @Description   : Intermediary key between code and the i18n translation file. Provides type safety and autocompletion.
  */
-import { CommonTranslationKeys } from '~/shared/enums/common-translation.enum'
-import { AuthTranslationKeys } from '~/modules/auth/constants/auth-translations.enum'
-import { FollowerTranslationKeys } from '~/modules/follower/constants/follower-translations.enum'
-import { UserTranslationKeys } from '~/modules/users/constants/user-translations.enum'
+
+import { CommonTranslationKeys, ValidationTranslationKeys, SuggestionTranslationKeys } from '~/shared/enums'
+import { AuthTranslationKeys } from '~/modules/auth/constants'
+import { UserTranslationKeys } from '~/modules/users/constants'
+import { FollowerTranslationKeys } from '~/modules/follower/constants'
+import { CommentTranslationKeys } from '~/modules/comments/constants'
 
 export type TranslationKeys =
   | CommonTranslationKeys
   | AuthTranslationKeys
   | UserTranslationKeys
   | FollowerTranslationKeys
+  | CommentTranslationKeys
+  | ValidationTranslationKeys
+  | SuggestionTranslationKeys
 
 export const TRANSLATION_KEYS = {
   ...CommonTranslationKeys,
   ...AuthTranslationKeys,
   ...UserTranslationKeys,
   ...FollowerTranslationKeys,
+  ...CommentTranslationKeys,
+  ...ValidationTranslationKeys,
+  ...SuggestionTranslationKeys
 } as const
+
